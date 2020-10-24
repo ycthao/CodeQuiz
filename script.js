a7f2cb5f0dc24a2591a2caeeab5a5a07e54dae32
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-10-24 15:45:14 -0500

    Adding gaveOver function to end game when timer goes to 0

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let startEl = document.getElementById("start");
 let instructionEl = document.getElementById("instruction");
 let quizQuestionEl = document.getElementById("start-quiz");
 let timerEl = document.getElementById("time");
+let gameOverEl = document.getElementById("save-score");
 
 function startQuiz() {
     // Call just to see if the on click work.  will delete once everything works
@@ -34,10 +35,25 @@ function timeCountdown() {
 
     if (time <= 0){
         timerEl.textContent = 0;
+        gameOver();
     }
 
 
   }
+
+function gameOver()
+{
+    // if time = 0  then this screen need to show
+    // to show game over screen, need to remove question
+    
+    // hide questions div
+    quizQuestionEl.setAttribute("class", "hide");
+    
+    // unhide gave over screen
+    gameOverEl.removeAttribute("class");
+
+    // if all answers are correct show this screen
+}
 
 
 startEl.onclick = startQuiz;
