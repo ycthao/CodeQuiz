411273c2012a7577aa4ab7dcb17ea567446f9874
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-10-24 15:11:27 -0500

    Hiding the instruction div

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-let start = document.getElementById("start");
-
+let startEl = document.getElementById("start");
+let instructionEl = document.getElementById("instruction");
 
 
 function startQuiz() {
+    // Call just to see if the on click work.  will delete once everything works
+    //alert("It works!");
 
-    alert("It works!");
+    // once start quiz is open
+    // need to hide quiz instruction and show questions
+    // also need to start timer
+    
+    // Hide instruction when start button is clicked
+    instructionEl.setAttribute("class", "hide");
 
 }
 
-start.onclick = startQuiz;
+startEl.onclick = startQuiz;
