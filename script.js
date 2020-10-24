39f0a09fc079ed56503cc47e44250b334c6ea943
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-10-24 15:38:06 -0500

    Adding timer

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+let timerId;
+let time = 10;
+
 let startEl = document.getElementById("start");
 let instructionEl = document.getElementById("instruction");
 let quizQuestionEl = document.getElementById("start-quiz");
+let timerEl = document.getElementById("time");
 
 function startQuiz() {
     // Call just to see if the on click work.  will delete once everything works
@@ -16,6 +20,24 @@ function startQuiz() {
     // Unhide quiz section by taking hide class off
     quizQuestionEl.removeAttribute("class");
 
+    // start timer
+    timerId = setInterval(timeCountdown, 1000);
+
+
+
 }
+
+function timeCountdown() {
+    // update time
+    time--;
+    timerEl.textContent = time;
+
+    if (time <= 0){
+        timerEl.textContent = 0;
+    }
+
+
+  }
+
 
 startEl.onclick = startQuiz;
