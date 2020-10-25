079b0a0c64f4b5647c7ffcf61bcb93ca4fc80885
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-10-24 23:13:53 -0500

    trying to add questiond and game over screen

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+let currentQuestionIndex = 0;
 let timerId;
-let time = 10;
+let time = questions.length * 15;
 
 let startEl = document.getElementById("start");
 let instructionEl = document.getElementById("instruction");
 let quizQuestionEl = document.getElementById("start-quiz");
 let timerEl = document.getElementById("time");
 let gameOverEl = document.getElementById("save-score");
+let choicesEl = document.getElementById("choices");
+let feedbackEl = document.getElementById("feedback");
+
 
 function startQuiz() {
     // Hide instruction when start button is clicked
@@ -16,8 +20,97 @@ function startQuiz() {
 
     // start timer
     timerId = setInterval(timeCountdown, 1000);
+    timerEl.textContent = time;
+    getQuestion();
 }
 
+function getQuestion() {
+    // get current question object from array
+    let currentQuestion = questions[currentQuestionIndex];
+    
+    // update title with current question
+    let titleEl = document.getElementById("question-title");
+    // print title in between h1 tag
+    titleEl.textContent = currentQuestion.title;
+    
+    // clear out any old question choices
+    choicesEl.innerHTML = "";
+    
+    // loop over choices
+    currentQuestion.choices.forEach(function(choice, i) {
+        // create new button for each choice
+        let questionChoice = document.createElement("button");
+        questionChoice.setAttribute("class", "choice");
+        questionChoice.setAttribute("value", choice);
+        
+        questionChoice.textContent = i + 1 + ". " + choice;
+        
+        // attach click event listener to each choice
+        questionChoice.onclick = questionClick;
+        
+        // display on the page
+        choicesEl.appendChild(questionChoice);
+    });
+}
+
+function questionClick() {
+    // check if user guessed wrong
+    if (this.value !== questions[currentQuestionIndex].answer) {
+        // penalize time -15 seconds
+        time -= 15;
+        
+        // change time to display 0 once it reach 0
+        // else it'll and show negative time
+        if (time < 0) {
+            time = 0;
+        }
+        timerEl.textContent = time;
+        
+        // diplay when the answer is wrong
+        feedbackEl.textContent = "Wrong!";
+        } else {
+        // display when the answer if right    
+        feedbackEl.textContent = "Correct!";
+        }
+        
+        // flash right/wrong feedback on page for half a second
+        feedbackEl.setAttribute("class", "feedback");
+        setTimeout(function() {
+            feedbackEl.setAttribute("class", "feedback hide");
+        }, 1000);
+        
+        // move to next question
+        currentQuestionIndex++;
+        
+        // check if we've run out of questions
+        if (currentQuestionIndex === questions.length) {
+            gameOver();
+        } else {
+            getQuestion();
+        }
+}
+    
+function gameOver()
+{
+    // stop timer
+    clearInterval(timerId);
+
+    // if time = 0  then this screen need to show
+    // to show game over screen, need to remove question
+    
+    // hide questions section
+    quizQuestionEl.setAttribute("class", "hide");
+    
+    
+    let finalScoreEl = document.getElementById("final-score");
+    finalScoreEl.textContent = time;
+
+    // show gave over screen
+    gameOverEl.removeAttribute("class");
+    
+    // if all answers are correct show this screen
+}
+    
 function timeCountdown() {
     // update time
     time--;
@@ -30,21 +123,10 @@ function timeCountdown() {
         // Calling gameOver function to end game when time goes to 0
         gameOver();
     }
-  }
-
-function gameOver()
-{
-    // if time = 0  then this screen need to show
-    // to show game over screen, need to remove question
-    
-    // hide questions div
-    quizQuestionEl.setAttribute("class", "hide");
-    
-    // unhide gave over screen
-    gameOverEl.removeAttribute("class");
-
-    // if all answers are correct show this screen
 }
+    
+
+
 
 
 startEl.onclick = startQuiz;
